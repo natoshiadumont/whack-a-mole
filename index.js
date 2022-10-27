b5e5f1777070400a92732cd894b4abea560450e5
Natoshia Dumont <natoshia.dumont@gmail.com>
2022-10-26 19:37:16 -0500

    toggle moles feature

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ let score = 0;
 let scoreValue = document.getElementById('score');
 let wam = document.getElementById('whack-a-mole');
 let holes = document.getElementsByClassName('hole');
-const randomHoleIndex = Math.floor(Math.random() * holes.length);
-holes[randomHoleIndex].classList.toggle("mole");
 
+
+
+setInterval( ()=> {
+const randomHoleIndex = Math.floor(Math.random() * holes.length);  
+holes[randomHoleIndex].classList.toggle("mole");
+}, 500);
+
+
+const gameArea = document.getElementById('whack-a-mole');
+gameArea.addEventListener('click', function(clickEvent) {
+  if (clickEvent.target.matches('.mole')) {
+    // we hit a mole!
+  }
+});
